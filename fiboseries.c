a65f06ee0b4b16b84c416989ed25a2e0f78dedef
anuja-patil98 <59799600+anuja-patil98@users.noreply.github.com>
2020-01-12 21:40:26 +0530

    Update fiboseries.c

--- fiboseries.c
+++ fiboseries.c
@@ -5,7 +5,7 @@ int main()
     int a=0,b=1,c,i,no;
   printf("Enter the no of element:");
   scanf("%d",&no);
-  printf("\n%d %d",a,b);
+  printf("\n%d\t%d\t",a,b);
   for(i=2;i<no;i++)
   {
     c=a+b;
