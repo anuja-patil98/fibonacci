5a15001accb58a26f2ec804a880bec98554ce7fb
anuja-patil98 <59799600+anuja-patil98@users.noreply.github.com>
2020-01-12 21:35:53 +0530

    Create fiboseries.c

---
+++ fiboseries.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=0,b=1,c,i,no;
+  printf("Enter the no of element:");
+  scanf("%d",&no);
+  printf("\n%d %d",a,b);
+  for(i=2;i<no;i++)
+  {
+    c=a+b;
+    printf("%d",c);
+    a=b;
+    b=c;
+  }
+
+    return 0;
+}
